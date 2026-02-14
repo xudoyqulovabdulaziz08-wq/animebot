7aa7795a27997789b7d028ff35b1e1ba56e3a7b2
Abdulaziz Xudoyqulov <xudoyqulovabdulaziz08@gmail.com>
2026-02-14 14:01:55 +0500

    Update run.py

--- run.py
+++ run.py
@@ -7,16 +7,10 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 
 from main import start_bot  # Botni ishga tushirish funksiyasi
-from web.app import app     # Flask (app.py) obyekti
-import uvicorn                  # Flaskni asinxron ishga tushirish uchun
+ # Flask (app.py) obyekti
+              # Flaskni asinxron ishga tushirish uchun
 
-async def run_flask():
-    """Flaskni asinxron rejimda ishga tushirish"""
-    # Render portni o'zi beradi, agar bo'lmasa 5000 ni oladi
-    port = int(os.environ.get("PORT", 5000)) 
-    config = uvicorn.Config(app, host="0.0.0.0", port=port, loop="asyncio")
-    server = uvicorn.Server(config)
-    await server.serve()
+
 
 async def main():
     """Bot va Web qismni parallel yurgizish"""
@@ -32,6 +26,7 @@ if __name__ == "__main__":
     except (KeyboardInterrupt, SystemExit):
         print("🛑 Tizim to'xtatildi")
   
+
 
 
 
