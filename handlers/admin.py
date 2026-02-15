9910e69f950f5f5fca4a5da87a536f1bc0c14479
Abdulaziz Xudoyqulov <xudoyqulovabdulaziz08@gmail.com>
2026-02-15 19:24:46 +0500

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -3,31 +3,33 @@ from services.user_service import get_user_status
 from keyboard.admin_kb import get_admin_kb
 from telegram import Update
 from config import MAIN_ADMIN_ID
+from database import async_session
+
 
 async def admin_panel_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.effective_user.id
-    
+    async with async_session() as session:
+
     # Foydalanuvchi statusini tekshiramiz
-    # Sessiya va MAIN_ADMIN_ID mavjudligiga ishonch hosil qiling
-    status = await get_user_status(session, user.id, MAIN_ADMIN_ID)
+        status = await get_user_status(session, user_id, MAIN_ADMIN_ID )
     
-    if status in ["main_admin", "admin"]:
-        is_main = (status == "main_admin")
+        if status in ["main_admin", "admin"]:
+            is_main = (status == "main_admin")
         
-        # Log yozish qismi (INSERT INTO admin_logs) butunlay olib tashlandi 🚀
+             # Log yozish qismi (INSERT INTO admin_logs) butunlay olib tashlandi 🚀
         
-        admin_info = "👑 <b>Bosh Admin Paneli</b>" if is_main else "👨‍💻 <b>Admin Paneli</b>"
-        text = (
-            f"{admin_info}\n\n"
-            "Botni boshqarish va statistika bilan tanishish uchun quyidagi bo'limlardan birini tanlang:\n\n"
-            "<i>Eslatma: Tizim barqaror ishlamoqda.</i>"
-        )
+            admin_info = "👑 <b>Bosh Admin Paneli</b>" if is_main else "👨‍💻 <b>Admin Paneli</b>"
+            text = (
+                f"{admin_info}\n\n"
+                "Botni boshqarish va statistika bilan tanishish uchun quyidagi bo'limlardan birini tanlang:\n\n"
+            "   <i>Eslatma: Tizim barqaror ishlamoqda.</i>"
+            )
         
-        await update.message.reply_text(
-            text=text,
-            reply_markup=get_admin_kb(is_main),
-            parse_mode="HTML"
-        )
-    else:
-        await update.message.reply_text("❌ <b>Sizda ushbu bo'limga kirish huquqi yo'q!</b>", parse_mode="HTML")
+            await update.message.reply_text(
+                text=text,
+                reply_markup=get_admin_kb(is_main),
+                parse_mode="HTML"
+            )
+        else:
+            await update.message.reply_text("❌ <b>Sizda ushbu bo'limga kirish huquqi yo'q!</b>", parse_mode="HTML")
 
