bd05b21a938f9fb3d70803348a07aa183bf9b1a0
Abdulaziz Xudoyqulov <Xudoyqulovabdulaziz@example.com>
2026-08-15 08:33:43 +0500

    yangilash96

---
+++ handlers/animelarim/korishlar_tarixi/korishlar.py
@@ -0,0 +1,60 @@
+
+
+
+from aiogram import Router, F
+from aiogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.exceptions import TelegramBadRequest
+
+router = Router()
+
+
+def get_history_keyboard() -> InlineKeyboardMarkup:
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                text="▶️ Tugatilgan animelar",
+                callback_data="history_completed"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="⏯️ Ko‘rilayotgan animelar",
+                callback_data="history_watching"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="⬅️ Orqaga",
+                callback_data="cabinet_back"
+            )
+        ]
+    ]
+
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
+
+
+@router.callback_query(F.data == "cabinet_history")
+async def show_watching_history(callback: CallbackQuery):
+
+    caption = (
+        "📜 <b>Ko‘rish tarixi</b>\n\n"
+        "<blockquote >"
+        "Bu bo‘limda ko‘rgan va hozir ko‘rayotgan "
+        "animelaringiz saqlanadi."
+        "</blockquote>\n\n"
+        "Kerakli bo‘limni tanlang."
+    )
+
+    keyboard = get_history_keyboard()
+
+    try:
+        await callback.message.edit_caption(
+            caption=caption,
+            reply_markup=keyboard,
+            parse_mode="HTML"
+        )
+
+        await callback.answer()
+
+    except TelegramBadRequest:
+        await callback.answer()
